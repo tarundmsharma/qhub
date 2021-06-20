866eb19e9556ce1e6a6e3ebc5ebd2338a08598bc
tarundmsharma <tarun.sharma@elastech.com>
2021-06-20 19:14:13 +0530

    ain

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+qhub init gcp --project qhub-deployments --domain qhub.dev --ci-provider github-actions --auth-provider auth0
+--auth-auto-provision --repository github.com/quansight/qhub --repository-auto-provision
